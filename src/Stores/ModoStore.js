2cf045d07a5008fcd9b3c08eaf14eb0e1b18cf46
James Kropp <james-kropp@hotmail.com>
2018-02-24 12:56:20 -0800

    Add other MODO calls

--- src/Stores/ModoStore.js
+++ src/Stores/ModoStore.js
@@ -1,16 +1,48 @@
 import FetchResource from '../Resources/FetchResource';
 
 class ModoStore {
-  car_list = {};
   nearby = {};
+  car_list = {};
+  availability = {};
   cost = {};
   isLoading = true;
 
+  getNearby(lat, lng) {
+    return new Promise(resolve => {
+      this.isLoading = true;
+      FetchResource.callModo(`nearby?lat=${lat}&long=${lng}`)
+        .then(res => {
+          this.nearby = res.Response;
+          this.isLoading = false;
+          resolve();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+  }
+
   getCarList() {
     return new Promise(resolve => {
+      this.isLoading = true;
       FetchResource.callModo('car_list')
         .then(res => {
           this.car_list = res.Response;
+          this.isLoading = false;
+          resolve();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+  }
+
+  getAvailability() {
+    return new Promise(resolve => {
+      this.isLoading = true;
+      FetchResource.callModo('availability')
+        .then(res => {
+          this.availability = res.Response;
           this.isLoading = false;
           resolve();
         })
--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,48 @@ import ModoStore from '../Stores/ModoStore';
 import BasicMap from './Map/BasicMap';
 
 class App extends Component {
-  render() {
-    ModoStore.getCarList().then(() => {
-      console.log(ModoStore.car_list);
+  state = {
+    lat: 49.201,
+    lng: -122.91,
+    cars: {},
+    nearby: {},
+    available: {}
+  };
+
+  componentDidMount() {
+    this.handleLoadNearby();
+    // this.handleLoadCars();
+    // this.handleLoadAvailability();
+  }
+
+  handleLoadNearby(lat = null, lng = null) {
+    ModoStore.getNearby(this.state.lat, this.state.lng).then(() => {
+      if (ModoStore.isLoading === false) {
+        this.setState({ nearby: ModoStore.nearby });
+        console.log(this.state.nearby);
+      }
     });
+  }
+
+  handleLoadCars() {
+    ModoStore.getCarList().then(() => {
+      if (ModoStore.isLoading === false) {
+        this.setState({ cars: ModoStore.car_list });
+        console.log(this.state.cars);
+      }
+    });
+  }
+
+  handleLoadAvailability() {
+    ModoStore.getAvailability().then(() => {
+      if (ModoStore.isLoading === false) {
+        this.setState({ available: ModoStore.availability });
+        console.log(this.state.availability);
+      }
+    });
+  }
+
+  render() {
     return (
       <div className="App">
         <div> Hey </div>
