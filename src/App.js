869a67104a01cec5bc69d1b41afc5dcc03675ada
Aiden Lee <aiden.lee.jh@gmail.com>
2018-02-24 09:21:24 -0800

    test secrete.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          <p>hello</p>
+          <p>{window.api_key}</p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
