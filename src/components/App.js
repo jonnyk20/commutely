b930cf4e3597e9a4a6962ac1fbb8ac09984a40cd
Jonny Kalambay <>
2018-02-24 10:10:04 -0800

    directions render

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import MapWithSearchAndDirections from './Map/MapWithSearchAndDirections';
+import MapWithDirectionsRenderer from './Map/MapWithDirectionsRenderer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div> Hey </div>
-        <MapWithSearchAndDirections />
+        <MapWithDirectionsRenderer />
       </div>
     );
   }
