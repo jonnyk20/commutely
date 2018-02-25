d5501dac194fdb472274b15c989e18cde910595e
Aiden Lee <aiden.lee.jh@gmail.com>
2018-02-24 23:20:04 -0800

    added moment calculation and selected directions

--- src/components/App.js
+++ src/components/App.js
@@ -29,6 +29,7 @@ class App extends Component {
   }
 
   selectStep = stepId => {
+    console.log('selected: ', stepId);
     const newSteps = this.state.steps.map(step => {
       step.selected = step.id === stepId ? true : false;
       return step;
@@ -50,6 +51,7 @@ class App extends Component {
     this.setState({
       steps: newStepsArray
     });
+    console.log('step: ', this.state.steps);
   };
 
   setDirections = directions => {
@@ -109,6 +111,7 @@ class App extends Component {
                 {directions &&
                   directions.routes &&
                   <Directions
+                    selectStep={this.selectStep}
                     directions={this.state.directions}
                     steps={this.state.steps}
                   />}
